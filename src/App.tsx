4fc0269fd13795b766ebf979f4e9d13f1d31af58
vamsisai <vamsisai.karumuri@gmail.com>
2024-07-09 23:06:20 +0530

    feat:implement router and setup pages

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './App.css'
+import Signup from './pages/Signup';
+import Dashboard from './pages/Dashboard';
+
+const router = createBrowserRouter([
+  {
+    path: "/signup",
+    element: <Signup/>,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard/>,
+  }
+]);
+
+function App() {
+
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  )
+}
+
+export default App
